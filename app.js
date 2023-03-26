f1b47bf39a9595463a22fb1d8a3331c52e1d0824
viktoriaukr <116932692+viktoriaukr@users.noreply.github.com>
2023-03-25 22:35:40 -0500

    rating app

---
+++ app.js
@@ -0,0 +1,14 @@
+$("button").on("click", function () {
+  let title = $("#title").val();
+  let rating = $("#rating").val();
+  let movie = $("<div class='movie-title'></div>").text(`${title} ${rating}`);
+  let deleteBtn = $("<button class='btn'>X</button>");
+  movie.append(deleteBtn);
+  $("#title-list").append(movie);
+  $("#title").val("");
+  $("#rating").val("");
+
+  $(document).on("click", ".btn", function () {
+    $(this).parent().remove();
+  });
+});
